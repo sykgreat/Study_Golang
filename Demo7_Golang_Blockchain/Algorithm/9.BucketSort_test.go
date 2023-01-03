a2ee138874f779791b07f9b23a16beedec92eeef
showmaker123 <663992442@qq.com>
2023-01-03 16:00:32 +0800

    。。。

---
+++ Demo7_Golang_Blockchain/Algorithm/9.BucketSort_test.go
@@ -0,0 +1,12 @@
+package Algorithm
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	a := []int{1, 2, 3, 4, 5}
+	d := sort.Search(len(a), func(i int) bool { return a[i] >= 3 })
+	fmt.Println(d)
+}
---
+++ test/file_test.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"log"
+	"os"
+	"syscall"
+	"testing"
+)
+
+func Test_File(t *testing.T) {
+	path := "D:\\system\\Downloads\\CyberPunk\\Cyberpunk_Edgerunners1.mp4"
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		log.Fatalf("ioutil.ReadFile err: %v", err)
+	}
+	fileSys := fileInfo.Sys().(*syscall.Win32FileAttributeData)
+	fileAttributes := fileSys.FileAttributes
+	t.Log(fileAttributes)
+}
